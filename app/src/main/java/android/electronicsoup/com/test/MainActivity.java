abc1d6fee3b91915f8a8b2686810ef40d7dc6704
John Whitmore <johnfwhitmore@gmail.com>
2018-07-30 22:21:14 +0100

    Added CanL2Frame activity to ensure lib correct

--- app/src/main/java/android/electronicsoup/com/test/MainActivity.java
+++ app/src/main/java/android/electronicsoup/com/test/MainActivity.java
@@ -2,8 +2,11 @@ package android.electronicsoup.com.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.electronicsoup.com.cinnamonbun.CanL2Frame;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CanL2Frame mFrame = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
