9e27939d4dfc1efa8cb69ddd7c5fd261ce7c0975
John Whitmore <johnfwhitmore@gmail.com>
2018-08-01 22:05:04 +0100

    Add CinnamonBunExecption class.
    Signed-off-by: John Whitmore <johnfwhitmore@gmail.com>

---
+++ cinnamonbun/src/main/java/android/electronicsoup/com/cinnamonbun/CinnamonBunException.java
@@ -0,0 +1,8 @@
+package android.electronicsoup.com.cinnamonbun;
+
+public class CinnamonBunException extends Exception{
+
+    public CinnamonBunException(String message){
+		super(message);
+    }
+}
